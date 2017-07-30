f7e13942fdbe63a939567903c098418c652e5e93
Don Smith <don@antiantonym.com>
2017-07-30 12:04:19 +1200

    show more than one point

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 setupCanvas()
 resizeCanvas()
 window.addEventListener('resize', resizeCanvas, false)
+startAnimation()
 
 function setupCanvas () {
   const canvas = document.createElement('canvas')
@@ -14,6 +15,18 @@ function setupCanvas () {
   document.body.appendChild(canvas)
 }
 
+function resizeCanvas () {
+  const canvas = getCanvas()
+  const {width, height} = getWindowSize()
+  canvas.width = width
+  canvas.height = height
+  startAnimation()
+}
+
+function getCanvas () {
+  return document.getElementById('canvas')
+}
+
 function getWindowSize () {
   return {
     width: window.innerWidth,
@@ -21,27 +34,84 @@ function getWindowSize () {
   }
 }
 
-function getCanvas () {
-  return document.getElementById('canvas')
-}
-
-function resizeCanvas () {
-  const canvas = getCanvas()
+function startAnimation () {
   const {width, height} = getWindowSize()
-  canvas.width = width
-  canvas.height = height
-  draw()
+  const points = getPoints(width, height)
+  const maxStep = 10
+  let rev = false
+  let step = 10
+  // setInterval(() => {
+    step = rev ? ++step : --step
+    draw(points, step)
+    rev = step > maxStep || step < 0 ? !rev : rev
+  // }, 200)
 }
 
-function draw () {
+function getPoints (width, height, palette) {
+  // [size, x, y, r, g, b]
+  const points = []
+  const count = 100
+
+  for (let i = 0; i < count; i++) {
+    points.push(getPoint(width, height, palette))
+  }
+  // points.push([500, 100, 300, 255, 0, 0])
+  // points.push([200, 100, 100, 0, 255, 0])
+  // points.push([100, 400, 300, 0, 0, 255])
+  // points.push([200, 100, 500, 255, 255, 0])
+  return points
+}
+
+function getPoint (width, height, palette) {
+  const point = []
+  // const point = [500, 100, 300, 255, 0, 0]
+  point.push(getRandomSize(width, height))
+  Array.prototype.push.apply(point, getRandomLocation(width, height))
+  Array.prototype.push.apply(point, getRandomColor(palette))
+  return point
+}
+
+function getRandomSize (width, height) {
+  const min = 200
+  const max = Math.min(width / 2, height / 2)
+  const size = Math.random() * (max - min) + min
+  return Math.floor(size)
+}
+
+function getRandomLocation (width, height) {
+  const min = -100
+  const location = []
+  const x = Math.random() * (width - min) + min
+  const y = Math.random() * (height - min) + min
+  location.push(Math.floor(x))
+  location.push(Math.floor(y))
+  return location
+}
+
+function getRandomColor (palette) {
+  const color = []
+  color.push(Math.floor(Math.random() * 255))
+  color.push(Math.floor(Math.random() * 255))
+  color.push(Math.floor(Math.random() * 255))
+  return color
+}
+
+function draw (points, step) {
   const ctx = getCanvas().getContext('2d')
   const {width, height} = getWindowSize()
   ctx.fillStyle = 'rgb(215,197,174)'
   ctx.fillRect(0, 0, width, height)
 
-  const gradient = ctx.createRadialGradient(300, 300, 200, 300, 300, 0)
-  gradient.addColorStop(0, 'rgba(255, 255, 255, 0)')
-  gradient.addColorStop(1, 'rgba(255, 0, 0, 0.25)')
-  ctx.fillStyle = gradient
-  ctx.fillRect(100, 100, 400, 400)
+  points.forEach((point, idx) => {
+    const opacity = step * 0.1 / 2
+    const [size, x, y, r, g, b] = point
+    const gradRad = Math.floor(size * 0.5)
+    const gradX = Math.floor(size * 0.5 + x)
+    const gradY = Math.floor(size * 0.5 + y)
+    const gradient = ctx.createRadialGradient(gradX, gradY, gradRad, gradX, gradY, 0)
+    gradient.addColorStop(0, 'rgba(255, 255, 255, 0)')
+    gradient.addColorStop(1, `rgba(${r}, ${g}, ${b}, ${opacity})`)
+    ctx.fillStyle = gradient
+    ctx.fillRect(x, y, size, size)
+  })
 }
